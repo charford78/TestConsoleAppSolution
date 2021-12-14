76836cc57c612873364549da76db07e9f0f461ff
Charlie Ford <charles.ford1978@gmail.com>
2021-12-13 23:11:34 -0500

    Initial commit

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("To the C# World...");
+
+            int NumberOfStudents = 17;
+            var TotalInClass = NumberOfStudents + 2;
+            Console.WriteLine($"Number of students is {NumberOfStudents}, total is {TotalInClass}");
+            var MyName = "Charlie";
+            Console.WriteLine($"{MyName} is one of the {NumberOfStudents} students in the class.");
+            Console.WriteLine($"There are {TotalInClass - NumberOfStudents} in the class that are not students.");
+        }
+    }
+}
